c6abdc832cc15f250ce8f3748ff2302f61436e11
Sena Hong <senahong@senaui-MacBook-Air.local>
2021-11-23 14:40:32 +0900

    그랩마켓 서버 시작

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ const server = http.createServer(function (req, res) {
   const method = req.method;
   if (path === "/products") {
     if (method === "GET") {
-      res.writeHead(200, { "Content=Type": "application/json" });
+      res.writeHead(200, { "Content-Type": "application/json" });
       const products = JSON.stringify([
         {
           name: "농구공",
           price: 5000,
         },
       ]);
+      res.end(products);
     } else if (method === "POST") {
-      res.end("생성되었습니다");
+      res.end("생성되었습니다!");
     }
   }
-  res.end(products);
+  res.end("Good Bye");
 });
 
 server.listen(port, hostname);
